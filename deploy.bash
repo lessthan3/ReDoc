77fc2c4daeb7cf43a6f5dd0a374b6ddc873ff55c
jitcoder <sam@bytecodestudios.com>
2019-02-27 17:28:58 -0800

    jesus

---
+++ deploy.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# install stand-alone gsutil
+curl https://storage.googleapis.com/pub/gsutil.tar.gz --output gsutil.tar.gz
+tar xfz gsutil.tar.gz -C $HOME
+rm gsutil.tar.gz
+
+export PATH=$HOME/gsutil:${PATH}
+echo "$service_account_creds" > account.json
+
+cat << BOTO > .boto
+[Credentials]
+gs_service_key_file = account.json
+[Boto]
+https_validate_certificates = True
+[GoogleCompute]
+[GSUtil]
+content_language = en
+default_api_version = 2
+default_project_id = lessthan3
+[OAuth2]
+BOTO
+
+export BOTO_CONFIG=$(pwd)/.boto
+gsutil version -l
+
+gsutil -m cp -r bundles gs://maestro-developer-documentation-ci-test
+
+rm account.json
+rm .boto
